388146b5bc3e94497d14beb7fc887bc1cfe1a1e8
YeetTheAnson <113901821+YeetTheAnson@users.noreply.github.com>
2024-06-30 22:13:50 +0800

    Update BlueScreen.ps1

--- shortcutactivation/BlueScreen/BlueScreen.ps1
+++ shortcutactivation/BlueScreen/BlueScreen.ps1
@@ -1,6 +1,35 @@
 # Change directory to Music
 Set-Location -Path ([Environment]::GetFolderPath("MyMusic"))
 
+Function Set-Volume 
+{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [ValidateRange(0,100)]
+        [Int]
+        $volume
+    )
+
+    # Calculate number of key presses. 
+    $keyPresses = [Math]::Ceiling( $volume / 2 )
+    
+    # Create the Windows Shell object. 
+    $obj = New-Object -ComObject WScript.Shell
+    
+    # Set volume to zero. 
+    1..50 | ForEach-Object {  $obj.SendKeys( [char] 174 )  }
+    
+    # Set volume to specified level. 
+    for( $i = 0; $i -lt $keyPresses; $i++ )
+    {
+        $obj.SendKeys( [char] 175 )
+    }
+}
+
+# Call the function with the desired volume level
+Set-Volume -volume 100
+
+
 
 (New-Object System.Media.SoundPlayer "BlueScreen.wav").Play()
 Start-Sleep -Seconds 8
