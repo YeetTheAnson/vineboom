c7836337f8cc3e57bed26813ad9203ab4fb922e9
YeetTheAnson <113901821+YeetTheAnson@users.noreply.github.com>
2024-06-30 13:27:05 +0800

    Update BSI.ps1

--- BSI.ps1
+++ BSI.ps1
@@ -3,12 +3,13 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/YeetTheAnson/vineboom/
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/YeetTheAnson/vineboom/main/shortcutactivation/BlueScreen/BlueScreen.mp4" -OutFile "BlueScreen.mp4"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/YeetTheAnson/vineboom/main/shortcutactivation/BlueScreen/BlueScreen.ps1" -OutFile "BlueScreen.ps1"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/YeetTheAnson/vineboom/main/shortcutactivation/BlueScreen/BlueScreen.wav" -OutFile "BlueScreen.wav"
+
 $WshShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$PWD\BlueScreen - Shortcut.lnk")
-$Shortcut.TargetPath = "$PWD\BlueScreen.bat"
+$Shortcut = $WshShell.CreateShortcut("$PSScriptRoot\BlueScreen - Shortcut.lnk")
+$Shortcut.TargetPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BlueScreen.bat"
 $Shortcut.Arguments = ""
 $Shortcut.Description = "Shortcut to BlueScreen.bat"
-$Shortcut.WorkingDirectory = $PWD
+$Shortcut.WorkingDirectory = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs"
 $Shortcut.Hotkey = "Alt+S"
 $Shortcut.WindowStyle = 7
 $Shortcut.Save()
